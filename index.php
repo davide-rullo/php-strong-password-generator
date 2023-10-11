4c5ebda311598f75cbdaa7c32ae0096a4be09463
davide-rullo <davide.rullo01@gmail.com>
2023-10-11 15:38:12 +0200

    add password loop message

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ var_dump($password);
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        <h3>Password generata: <?php echo $password ?></h3>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
